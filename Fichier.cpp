a06f9fd24365179d711f3e30a6d9960735d5f6c0
badtommy67 <104507862+badtommy67@users.noreply.github.com>
2025-12-04 09:43:36 +0100

    modifications pour avoir 10 lignes / fonctions

--- Fichier.cpp
+++ Fichier.cpp
@@ -9,118 +9,111 @@ namespace fs = std::filesystem;
 
 Fichier::Fichier(string entree) : nom_entree(entree){}
 
+void Fichier::remplirMatriceDepuisFichier(ifstream& fichier, vector<vector<bool>>& matrice) {
+    int lignes = matrice.size();
+    int colonnes = matrice[0].size();
+    for (int i = 0; i < lignes; i++) {
+        for (int j = 0; j < colonnes; j++) {
+            char c;
+            fichier >> c;
+            matrice[i][j] = (c != '0'); // Si c'est pas '0', c'est vrai (1)
+        }
+    }
+}
+
 vector<vector<bool>> Fichier::lecture() {
     ifstream fichier(nom_entree.c_str());
     if (!fichier) {
         cout << "Erreur à l'ouverture !" << endl;
-        return {}; // renvoie une matrice vide en cas d'erreur
+        return {}; 
     }
 
-    int colonne;
-    int ligne;
-    fichier >> ligne >> colonne; // on met la ligne dans ligne et la colonne dans colonne
+    int lignes, colonnes;
+    fichier >> lignes >> colonnes;
 
-    vector<vector<bool>> matrice(ligne, vector<bool>(colonne)); // creation de la matrice
-
-    for (int i = 0; i < ligne; i++) {
-        for (int j = 0; j < colonne; j++) {
-            char c;
-            fichier >> c; // on recupere un caractere de la lecture, mais on cherche un booléen
-            bool val;
-            if (c=='0'){ // donc si le caractère est un 0 on défini le boléen à 0
-                val=0;
-            }else{
-                val=1;
-            }
-            matrice[i][j] = val; // on met la valeur booléenne dans la matrice
-        }
-    }
+    vector<vector<bool>> matrice(lignes, vector<bool>(colonnes));
+    remplirMatriceDepuisFichier(fichier, matrice);
+    
     fichier.close();
     return matrice;
 }
 
-void Fichier::ecriture(const vector<vector<bool>>& matriceEtat){
+fs::path Fichier::preparerDossierSortie() {
     fs::path chemin_entree(nom_entree);
     string nom_dossier = chemin_entree.stem().string() + "_out";
     fs::path dossier_cible = fs::path("Fichiers_sortie") / nom_dossier;
 
-    if (!fs::exists(dossier_cible)) { // par acquis de conscience, on créer le dossier s'il n'existe pas encore.
+    if (!fs::exists(dossier_cible)) {
         fs::create_directories(dossier_cible);
     }
-    int i = 1; // ce compteur nous permet de savoir quel est le prochain numero de fichier disponible
-    fs::path chemin_final;
+    return dossier_cible;
+}
 
-    bool trouve=false;
-
-    while (trouve==false) {
+fs::path Fichier::trouverFichierDispo(const fs::path& dossier) {
+    int i = 1;
+    while (true) {
         string nom_fichier = "iteration" + to_string(i) + ".txt";
-        chemin_final = dossier_cible / nom_fichier;
-
-        // si le fichier existe, on teste le suivant
-        if (fs::exists(chemin_final)) {
-            i++;
-        } else {
-            // si le fichier n'existe pas, parfait ! on le prend !
-            trouve=true;
-        }
+        fs::path chemin = dossier / nom_fichier;
+        if (!fs::exists(chemin)) return chemin;
+        i++;
     }
-    ofstream flux(chemin_final);
-    // on va ecrire la matrice dans le fichier
-    for (const auto& ligne : matriceEtat) {
-        for (int i = 0; i < ligne.size(); i++) {
-            bool val = ligne[i]; // On récupère la valeur
-            if (val == true) {
-                flux << '1';
-            } else {
-                flux << '0';
-            }
-        flux << ""; // On ajoute l'espace
+}
+
+void Fichier::ecrireContenuMatrice(ofstream& flux, const vector<vector<bool>>& matrice) {
+    for (const auto& ligne : matrice) {
+        for (bool val : ligne) {
+            if (val) flux << '1';
+            else flux << '0';
+            flux << "";
         }
         flux << endl;
     }
+}
+
+void Fichier::ecriture(const vector<vector<bool>>& matriceEtat) {
+    fs::path dossier = preparerDossierSortie();
+    fs::path chemin_final = trouverFichierDispo(dossier);
+    ofstream flux(chemin_final);
+    ecrireContenuMatrice(flux, matriceEtat);
     flux.close();
 }
 
-void Fichier::creationDossier(){
+void Fichier::creationDossier() {
     fs::path chemin_source(nom_entree);
-    string nom_dossier = chemin_source.stem().string();
-
-    nom_dossier += "_out";  // ajouter "_out"
-    fs::path chemin_complet = fs::path("Fichiers_sortie") / nom_dossier; // ici on va creer le chemin complet
+    string nom_dossier = chemin_source.stem().string() + "_out";
+    fs::path chemin_complet = fs::path("Fichiers_sortie") / nom_dossier;
 
     try {
         if (fs::create_directories(chemin_complet))
             cout << "Dossier '" << chemin_complet << "' créé avec succès !" << endl;
-    }catch (fs::filesystem_error& e) {
-        cout << "Erreur lors de la création du dossier : " << e.what() << endl;
+    } catch (fs::filesystem_error& e) {
+        cout << "Erreur création dossier : " << e.what() << endl;
     }
 }
 
-vector<vector<bool>> Fichier::aleatoire(int lignes, int colonnes){
-    fs::path chemin_entree(nom_entree);
+vector<vector<bool>> Fichier::genererMatriceAleatoire(int lignes, int colonnes) {
     vector<vector<bool>> matrice(lignes, vector<bool>(colonnes));
     srand(time(0));
+    
     for (int x = 0; x < lignes; ++x) {
         for (int y = 0; y < colonnes; ++y) {
-                matrice[x][y]=rand() % 2;
+            matrice[x][y] = rand() % 2;
         }
     }
-    ofstream flux(nom_entree);
-    // on va ecrire la matrice dans le fichier
-    flux<<lignes<<" "<<colonnes<<endl;
-    for (const auto& ligne : matrice) {
-        for (int i = 0; i < ligne.size(); i++) {
-            bool val = ligne[i]; // On récupère la valeur
-            if (val == true) {
-                flux << '1';
-            } else {
-                flux << '0';
-            }
-        flux << ""; // On ajoute l'espace
-        }
-        flux << endl;
-    }
-    flux.close();
+    return matrice;
+}
 
+void Fichier::sauvegarderConfigurationInitiale(const vector<vector<bool>>& matrice) {
+    ofstream flux(nom_entree);
+    // on écrit les dimensions en entête pour pouvoir etre reprises par la suite
+    flux << matrice.size() << " " << matrice[0].size() << endl;
+    
+    ecrireContenuMatrice(flux, matrice);
+    flux.close();
+}
+
+vector<vector<bool>> Fichier::aleatoire(int lignes, int colonnes) {
+    vector<vector<bool>> matrice = genererMatriceAleatoire(lignes, colonnes);
+    sauvegarderConfigurationInitiale(matrice);
     return matrice;
 }
